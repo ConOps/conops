317208e3a7d9cb9e94c744bb9e52c410531b650e
Marit Zelinsky <maritzelinsky@me.com>
2019-10-07 15:58:44 -0500

    adds nav drop down options

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,14 +2,14 @@ import { combineReducers } from 'redux';
 import errors from './errorsReducer';
 import loginMode from './loginModeReducer';
 import user from './userReducer';
-import AttendeeDetails from './AttendeeDetailsReducer';
-import AttendeesCheckIn from './AttendeesCheckInReducer';
-import AttendeesOrderId from './AttendeesOrderIdReducer';
-import Conventions from './ConventionsReducer';
-import Events from './EventsReducer';
-import HomePage from './HomePageReducer';
-import Tags from './TagsReducer';
-import Location from './LocationReducer'
+// import AttendeeDetails from './AttendeeDetailsReducer';
+// import AttendeesCheckIn from './AttendeesCheckInReducer';
+// import AttendeesOrderId from './AttendeesOrderIdReducer';
+// import Conventions from './ConventionsReducer';
+// import Events from './EventsReducer';
+// import HomePage from './HomePageReducer';
+// import Tags from './TagsReducer';
+// import Location from './LocationReducer'
 
 
 // rootReducer is the primary reducer for our entire project
@@ -22,14 +22,14 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
   user, // will have an id and username if someone is logged in
-  AttendeeDetails,
-  AttendeesCheckIn,
-  AttendeesOrderId,
-  Conventions,
-  Events,
-  HomePage,
-  Tags,
-  Location
+  // AttendeeDetails,
+  // AttendeesCheckIn,
+  // AttendeesOrderId,
+  // Conventions,
+  // Events,
+  // HomePage,
+  // Tags,
+  // Location
 });
 
 export default rootReducer;
