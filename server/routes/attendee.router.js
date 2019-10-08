f566149501f1ed85bc6f29182fe54f21a9cbda13
Dustin Fedie <dlfedie@gmail.com>
2019-10-08 11:29:51 -0500

    adds checked-in and pre-registered GET routes to attendee table!

--- server/routes/attendee.router.js
+++ server/routes/attendee.router.js
@@ -34,7 +34,7 @@ router.get('/', rejectUnauthenticated, async (req, res) => {
     }
 });
 
-//GET route for walk-in attendees
+//GET route for walk-in attendees, not yet checked-in
 router.get('/walk_ins', rejectUnauthenticated, async (req, res) => {
     console.log('in attendee walk-in GET route');
     //go get walk-in list of attendees, make sure to grab from current convention!
@@ -61,6 +61,63 @@ router.get('/walk_ins', rejectUnauthenticated, async (req, res) => {
         connection.release();
     }
 });
+
+//GET route for checked-in attendees
+router.get('/checked_in', rejectUnauthenticated, async (req, res) => {
+    console.log('in attendee checked-in GET route');
+    //go get checked-in list of attendees, make sure to grab from current convention!
+    const connection = await pool.connect();
+    try {
+        await connection.query('BEGIN');
+        //this will grab the convention ID that is the biggest; ie, the last created convention
+        const queryCon = `SELECT MAX("ConventionID") AS convention FROM "Convention";`;
+        //save result so we can use it in the followup query
+        const result = await connection.query(queryCon);
+        //set a variable to the top con ID
+        const conventionId = result.rows[0].convention;
+        //this query grabs the walk ins; no order ID and not checked in yet
+        const queryText = `SELECT * FROM "Attendee" WHERE "ConventionID" = $1 AND "CheckInDate" IS NOT NULL;`;
+        //use top con ID to find our attendees for that convention
+        const attendeeResult = await connection.query(queryText, [conventionId]);
+        await connection.query('COMMIT');
+        res.send(attendeeResult.rows);
+    } catch (error) {
+        await connection.query('ROLLBACK');
+        console.log('error in attendee walk-in GET', error);
+        res.sendStatus(500);
+    } finally {
+        connection.release();
+    }
+});
+
+//GET route for preregistered attendees, not yet checked-in
+router.get('/pre_registered', rejectUnauthenticated, async (req, res) => {
+    console.log('in attendee checked-in GET route');
+    //go get preregistered list of attendees, make sure to grab from current convention!
+    const connection = await pool.connect();
+    try {
+        await connection.query('BEGIN');
+        //this will grab the convention ID that is the biggest; ie, the last created convention
+        const queryCon = `SELECT MAX("ConventionID") AS convention FROM "Convention";`;
+        //save result so we can use it in the followup query
+        const result = await connection.query(queryCon);
+        //set a variable to the top con ID
+        const conventionId = result.rows[0].convention;
+        //this query grabs the walk ins; no order ID and not checked in yet
+        const queryText = `SELECT * FROM "Attendee" WHERE "ConventionID" = $1 AND "orderID" IS NOT NULL AND "CheckInDate" IS NULL;`;
+        //use top con ID to find our attendees for that convention
+        const attendeeResult = await connection.query(queryText, [conventionId]);
+        await connection.query('COMMIT');
+        res.send(attendeeResult.rows);
+    } catch (error) {
+        await connection.query('ROLLBACK');
+        console.log('error in attendee walk-in GET', error);
+        res.sendStatus(500);
+    } finally {
+        connection.release();
+    }
+});
+
 
 /**
  * POST route template
