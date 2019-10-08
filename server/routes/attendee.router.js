27b0fb76957287a3f57b7cfe6872610b9d855cbf
Dustin Fedie <dlfedie@gmail.com>
2019-10-08 11:09:50 -0500

    adds walk in get route

--- server/routes/attendee.router.js
+++ server/routes/attendee.router.js
@@ -3,9 +3,10 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+//GET ROUTES
+//GET routes are ok for all users to see; GET routes won't need to check authorization. they will check authentication.
+
+//GET route for ALL attendees
 router.get('/', rejectUnauthenticated, async (req, res) => {
     console.log('in attendee GET route');
     //go get full list of attendees, make sure to grab from current convention!
@@ -26,6 +27,34 @@ router.get('/', rejectUnauthenticated, async (req, res) => {
     } catch (error) {
         await connection.query('ROLLBACK');
         console.log('error in attendee GET', error);
+        res.sendStatus(500);
+    } finally {
+        connection.release();
+    }
+});
+
+//GET route for walk-in attendees
+router.get('/walk_ins', rejectUnauthenticated, async (req, res) => {
+    console.log('in attendee walk-in GET route');
+    //go get walk-in list of attendees, make sure to grab from current convention!
+    const connection = await pool.connect();
+    try {
+        await connection.query('BEGIN');
+        //this will grab the convention ID that is the biggest; ie, the last created convention
+        const queryCon = `SELECT MAX("ConventionID") AS convention FROM "Convention";`;
+        //save result so we can use it in the followup query
+        const result = await connection.query(queryCon);
+        //set a variable to the top con ID
+        const conventionId = result.rows[0].convention;
+        //this query grabs the walk ins; no order ID and not checked in yet
+        const queryText = `SELECT * FROM "Attendee" WHERE "ConventionID" = $1 AND "orderID" IS NULL AND "CheckInDate" IS NULL;`;
+        //use top con ID to find our attendees for that convention
+        const attendeeResult = await connection.query(queryText, [conventionId]);
+        await connection.query('COMMIT');
+        res.send(attendeeResult.rows);
+    } catch (error) {
+        await connection.query('ROLLBACK');
+        console.log('error in attendee walk-in GET', error);
         res.sendStatus(500);
     } finally {
         connection.release();
