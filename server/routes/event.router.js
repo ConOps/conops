8b0b13a929f92a3270f540b670ed5e7080f45da1
Marit Zelinsky <maritzelinsky@me.com>
2019-10-09 13:05:11 -0500

    adds GET route for events in event.router.js

--- server/routes/event.router.js
+++ server/routes/event.router.js
@@ -3,11 +3,35 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-
+// get all the events for the current convention
+router.get('/', rejectUnauthenticated, async (req, res) => {
+    console.log('in event GET route')
+    const connection = await pool.connect();
+    try {
+        await connection.query('BEGIN');
+        const queryCon = `SELECT MAX ("ConventionID") AS convention from "Convention";`;
+        const result = await connection.query(queryCon);
+        const conventionID = result.rows[0].convention;
+        const queryText = `SELECT "Event"."EventID", "Event"."ConventionID", "Event"."EventName", "Event"."EventStartTime", "Event"."EventEndTime", "Location"."LocationName", "Location"."LocationDescription", "Event"."IsCancelled", "Event"."EventDescription", "Sponsor"."SponsorName", "Event"."DateCreated", "Event"."DateLastModified", "Event"."EventModifiedNotes", array_agg("Tags"."TagName") AS "Tags" 
+                            FROM "Event"
+                            LEFT OUTER JOIN "Location" ON "Location"."LocationID" = "Event"."LocationID"
+                            LEFT OUTER JOIN "Sponsor" ON "Sponsor"."SponsorID" = "Event"."SponsorID"
+                            LEFT JOIN "EventTags" ON "EventTags"."Event_ID" = "Event"."EventID"
+                            LEFT JOIN "Tags" ON "Tags"."TagID" = "EventTags"."Tag_ID"
+                            WHERE "ConventionID" = $1
+                            GROUP BY "Event"."EventID", "Event"."EventName", "Location"."LocationName", "Location"."LocationDescription", "Sponsor"."SponsorName"
+                            ORDER BY "Event"."EventStartTime";`;
+        const eventResult = await connection.query(queryText, [conventionID]);
+        await connection.query('COMMIT');
+        console.log(eventResult.rows);
+        res.send(eventResult.rows);
+    } catch (error) {
+        await connection.query('ROLLBACK');
+        console.log('error in event GET', error)
+        res.sendStatus(500);
+    } finally {
+        connection.release();
+    }
 });
 
 /**
