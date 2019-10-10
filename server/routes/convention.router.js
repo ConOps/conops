0255a73dd0766d0ac168e77b4b63dd624b2f9b46
Dustin Fedie <dlfedie@gmail.com>
2019-10-10 11:13:30 -0500

    adds server-side PUT route for current convention

--- server/routes/convention.router.js
+++ server/routes/convention.router.js
@@ -20,9 +20,32 @@ router.get('/', rejectUnauthenticated, (req, res) => {
         })
 });
 
-/**
- * POST route template
- */
+//PUT route
+//PUT route for editing current convention (name, dates, etc)
+router.put('/', rejectUnauthenticated, async (req, res) => {
+    console.log('in convention PUT route');
+    const connection = await pool.connect();
+    try {
+        await connection.query('BEGIN');
+        const convention = req.body;
+        console.log('convention updates:', convention);
+        const queryText = `UPDATE "Convention"
+                            SET "OrganizationID" = $1, "ConventionName" = $2, "ConventionStartTime" = $3, "ConventionEndTime" = $4, "ConventionNews" = $5
+                            WHERE "ConventionID" = $6;`;
+        await connection.query(queryText, [convention.OrganizationID, convention.ConventionName, convention.ConventionStartTime, convention.ConventionEndTime, convention.ConventionNews, convention.ConventionID])
+        await connection.query('COMMIT');
+        res.sendStatus(200);
+    } catch(error) {
+        await connection.query('ROLLBACK');
+        console.log('error in convention PUT route:', error);
+        res.sendStatus(500);
+    } finally {
+        connection.release();
+    }
+});
+
+
+//POST route
 router.post('/', (req, res) => {
 
 });
