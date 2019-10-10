d3253efc209277d53998dfef4ec4c347305b2e7d
Dustin Fedie <dlfedie@gmail.com>
2019-10-10 10:38:31 -0500

    adds server-side GET for current convention.

--- server/routes/convention.router.js
+++ server/routes/convention.router.js
@@ -3,11 +3,21 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
 
+//GET route
+//GET route is only for grabbing current convention
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('in convention GET route');
+    //need to get latest convention, similar to in our attendee routes
+    const queryText = `SELECT * FROM "Convention" ORDER BY "ConventionID" DESC LIMIT 1;`;
+    pool.query(queryText)
+        .then(result => {
+            console.log('convention details:', result.rows[0]);
+            res.send(result.rows[0]);
+        }).catch(err => {
+            console.log('error in convention GET route:', err);
+            res.sendStatus(500);
+        })
 });
 
 /**
