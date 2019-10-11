9fc3d95c9a0b0f7985e06e0864f05f9eb5779e33
Christopher O'Bannon <Crobwan@gmail.com>
2019-10-11 13:41:19 -0500

    creates a middleware that will allow acces only to checkIns and Admin

--- server/modules/isAdminAuthentication-middleware.js
+++ server/modules/isAdminAuthentication-middleware.js
@@ -1,7 +1,7 @@
 const rejectNonAdmin = (req, res, next) => {
 //check if user access level is equal with Admin
 if(req.user.authorization ==4 ){
-    //they were authenticated!  User may do he next thing
+    //they were authenticated!  User may do the next thing
     //Note!  Admin can do anything!!
 next();
 }else{
