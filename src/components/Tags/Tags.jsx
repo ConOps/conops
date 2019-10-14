3f3bceaa9b6ce23196b3e70100a9882056b28a92
Christopher O'Bannon <Crobwan@gmail.com>
2019-10-14 09:49:33 -0500

    conditionally renders exucutable buttons in the tags and the conventinos componenens

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -27,52 +27,78 @@ class Tags extends Component {
 
     render() {
         return (
-            <div>
-                <h1 style={{ textAlign: "center" }}>Current Convention: 2DCON 2020</h1>
-                <h2 style={{ textAlign: "center" }}>Tags</h2>
-                <div style={{ textAlign: "right", marginRight: "2%" }}>
-                    <Button variant="contained" color="primary"  onClick={() => this.props.history.push(`/createTag`)}>Create New Tag</Button>
-                </div>
-                <MaterialTable
-                    title="Editable Example"
-                    columns={this.state.columns}
-                    options={{
-                        columnsButton: true,
-                        pageSize: 10,
-                        // headerStyle: { backgroundColor: 'blue', color: 'white' },
-                        pageSizeOptions: [10, 20, 50],
-                        toolbarButtonAlignment: "right",
-                        searchFieldAlignment: "left",
-                        showTitle: false
-                    }}
-                    data={this.props.reduxStore.TagsReducer}
-
-                    editable={{}}
-                    actions={[
-                        {
-                            icon: "edit",
-                            tooltip: "Edit",
-                            onClick: (event, rowData) => {
-                                this.props.history.push(`/edittag/${rowData.TagID}`);
-                            }
-                        },
-                        // {
-                        //     icon: "delete",
-                        //     tooltip: "Delete",
-                        //     onClick: (event, rowData) => {
-                        //         console.log(rowData.orderID);
-                        //         this.props.dispatch({
-                        //             type: "DELETE_TAG_INFO",
-                        //             payload: rowData.TagID
-                        //         });
-                        //     },
-
-                        // },
-
-                    ]}
-                />
-            
+          <div>
+            <h1 style={{ textAlign: "center" }}>
+              Current Convention: 2DCON 2020
+            </h1>
+            <h2 style={{ textAlign: "center" }}>Tags</h2>
+            <div style={{ textAlign: "right", marginRight: "2%" }}>
+             
+            {this.props.reduxStore.user.authorization == 4 && (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => this.props.history.push(`/createTag`)}
+              >
+                Create New Tag
+              </Button>
+            )}           
             </div>
+            {this.props.reduxStore.user.authorization == 4 ? (
+              <MaterialTable
+                title="Editable Example"
+                columns={this.state.columns}
+                options={{
+                  columnsButton: true,
+                  pageSize: 10,
+                  // headerStyle: { backgroundColor: 'blue', color: 'white' },
+                  pageSizeOptions: [10, 20, 50],
+                  toolbarButtonAlignment: "right",
+                  searchFieldAlignment: "left",
+                  showTitle: false
+                }}
+                data={this.props.reduxStore.TagsReducer}
+                editable={{}}
+                actions={[
+                  {
+                    icon: "edit",
+                    tooltip: "Edit",
+                    onClick: (event, rowData) => {
+                      this.props.history.push(`/edittag/${rowData.TagID}`);
+                    }
+                  }
+                  // {
+                  //     icon: "delete",
+                  //     tooltip: "Delete",
+                  //     onClick: (event, rowData) => {
+                  //         console.log(rowData.orderID);
+                  //         this.props.dispatch({
+                  //             type: "DELETE_TAG_INFO",
+                  //             payload: rowData.TagID
+                  //         });
+                  //     },
+
+                  // },
+                ]}
+              />
+            ) : (
+              <MaterialTable
+                title="Editable Example"
+                columns={this.state.columns}
+                options={{
+                  columnsButton: true,
+                  pageSize: 10,
+                  // headerStyle: { backgroundColor: 'blue', color: 'white' },
+                  pageSizeOptions: [10, 20, 50],
+                  toolbarButtonAlignment: "right",
+                  searchFieldAlignment: "left",
+                  showTitle: false
+                }}
+                data={this.props.reduxStore.TagsReducer}
+                editable={{}}
+              />
+            )}
+          </div>
         );
     }
 }
