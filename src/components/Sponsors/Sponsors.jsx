fd7add591282ac473980a16b5cf73dc42bba7112
Marit Zelinsky <maritzelinsky@me.com>
2019-10-14 13:36:30 -0500

    reverted location changes because someone is working in there currently

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { withStyles } from '@material-ui/core/styles';
+import MaterialTable from "material-table";
 
-const Sponsors = () => (
-    <div>
-        <p>Sponsors</p>
-    </div>
-);
+const styles = ({
+    root: {
+        margin: '15px',
+    }
+});
 
-export default Sponsors;
+class Sponsors extends Component {
+    componentDidMount() {
+        this.props.dispatch({ type: 'FETCH_SPONSORS' })
+    }
+
+    state = {
+        columns: [
+            { title: ""}
+        ]
+    }
+
+    render() {
+        return(
+            <div>
+                <p>Sponsors</p>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = reduxStore => {
+    return {
+        reduxStore
+    };
+};
+
+export default withStyles(styles)(connect(mapStateToProps)(Sponsors));
