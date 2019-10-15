9d83e2a034b2f724d66f3679c1d9fdeaefaa931b
Alex Smith <alexesmith@icloud.com>
2019-10-15 15:46:03 -0500

    the save button on the create location page now has a dialog box to confirm your save

--- src/components/Locations/CreateLocation.jsx
+++ src/components/Locations/CreateLocation.jsx
@@ -3,6 +3,13 @@ import { connect } from "react-redux";
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Paper from '@material-ui/core/Paper';
+import Draggable from 'react-draggable';
 
 const styles = ({
     root: {
@@ -10,21 +17,71 @@ const styles = ({
     },
 });
 
+function PaperComponent(props) {
+  return (
+    <Draggable>
+      <Paper {...props} />
+    </Draggable>
+  );
+}
+
 class CreateLocation extends Component {
+  state ={
+    openSave: false,
+    details: {},
+  }
+
+
+  handleCloseSave = () => {
+    this.setState({ openSave: false });
+  };
+
     handleCancel = () => {
         this.props.history.push('/locations');
     }
 
-    handleSave = (event) => {
-        alert('Location created.')
-        this.props.dispatch({
-            type: "ADD_LOCATION",
-            payload: this.props.details
-        })
+  saveLocation = () => {
+    this.props.dispatch({
+      type: "ADD_LOCATION",
+      payload: this.props.details
+    });
+    this.handleCloseSave();
+  }
+
+    handleSave = () => {
+      this.setState({
+        openSave: !this.state.openSave,
+        ...this.state.details, details: this.props.details
+      })
     }
     render() {
         return (
           <div>
+
+            <Dialog
+              open={this.state.openSave}
+              onClose={this.handleCloseSave}
+              PaperComponent={PaperComponent}
+              aria-labelledby="draggable-dialog-title"
+            >
+              <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+                Create Location?
+        </DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Are you sure that you would like to create this location?
+          </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.handleCloseSave} color="primary">
+                  Cancel
+          </Button>
+                <Button onClick={this.saveLocation} color="primary">
+                  Confirm
+          </Button>
+              </DialogActions>
+            </Dialog>
+
             <h1>Create Location</h1>
             <hr></hr>
             <TextField
