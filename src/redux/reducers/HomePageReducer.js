25bc25b1e76659f7f59652df7450a517d49d3ada
Christopher O'Bannon <Crobwan@gmail.com>
2019-10-15 12:07:25 -0500

    activates the homePageReducer

--- src/redux/reducers/HomePageReducer.js
+++ src/redux/reducers/HomePageReducer.js
@@ -1,4 +1,12 @@
+const homePageReducer = (state = [], action) => {
 
+    switch (action.type) {
+        case 'SET_NEWS':
+            return action.payload;
+            default:
+                return state;
+    }
+}
 
 
 
