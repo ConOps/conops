db204cdea2d089aff64596e6adb3b5cb6ba91a6d
Christopher O'Bannon <Crobwan@gmail.com>
2019-10-15 14:17:44 -0500

    creates a get route for news in the home router

--- server/routes/home.router.js
+++ server/routes/home.router.js
@@ -4,6 +4,11 @@ const { rejectNonAdmin } = require('../modules/isAdminAuthentication-middleware'
 const pool = require('../modules/pool');
 const router = express.Router();
 
-
+router.get('/', rejectUnauthenticated, (req, res) => {
+    queryText = `SELECT "ConventionNews" FROM "Convention" WHERE "ConventionID" = (SELECT MAX("ConventionID") FROM "Convention");`;
+    pool.query(queryText)
+        .then(res.send(result.rows))
+        .catch(res.sendStatus(500))
+})
 
 module.exports = router;
