2bc8901f43d61c9fabdf2751f417ee6788b586c7
Christopher O'Bannon <Crobwan@gmail.com>
2019-10-15 12:12:47 -0500

    activates home saga and creates a get route in it

--- src/redux/sagas/HomePageSaga.js
+++ src/redux/sagas/HomePageSaga.js
@@ -1,7 +1,24 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
-
-
+function * fetchNews(){
+    try {
+        const config = {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true,
+        };
+        const response = yield axios.get('/api/home/', config)
+        yield put({
+            type: 'SET_NEWS',
+            payload: response.data
+        })
+}catch(error){
+    console.log('error in fetchNews', error);
+    
+}
+}
+function * homePageSaga(){
+    yield takeLatest('FETCH_NEWS', fetchNews)
+}
 
 export default homePageSaga;
