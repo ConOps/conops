90c6e175c3fb46cede956f27fe0fc889bdf4fd72
Alex Smith <alexesmith@icloud.com>
2019-10-15 13:35:35 -0500

    added routing

--- src/components/LocationDetails/LocationDetails.jsx
+++ src/components/LocationDetails/LocationDetails.jsx
@@ -24,6 +24,18 @@ class LocationDetails extends Component {
         LocationIsActive: true,
     }
 
+    componentDidMount () {
+      this.fetchLocationDetails();
+    }
+
+    fetchLocationDetails = () => {
+      let id = this.props.match.params.id;
+      this.props.dispatch({
+        type: "FETCH_LOCATION_DETAILS",
+        payload: id
+      });
+    }
+
     handleBack = () => {
         this.props.history.push("/locations");
     };
@@ -50,6 +62,7 @@ class LocationDetails extends Component {
             <h1>Manage Location: {this.props.details.LocationName}</h1>
             <TextField
               label="Name"
+              InputLabelProps={{ shrink: this.props.details.LocationName }}
               className={this.props.classes.root}
               value={this.props.details.LocationName}
               onChange={event =>
@@ -61,6 +74,7 @@ class LocationDetails extends Component {
             ></TextField>
             <TextField
               label="Description"
+              InputLabelProps={{ shrink: this.props.details.LocationDescription }}
               className={this.props.classes.root}
               value={this.props.details.LocationDescription}
               onChange={event =>
--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -68,11 +68,11 @@ class Locations extends Component {
                     icon: "edit",
                     tooltip: "Edit Location",
                     onClick: (event, rowData) => {
-                      this.props.dispatch({
-                        type: "FETCH_LOCATION_DETAILS",
-                        payload: rowData.LocationID
-                      });
-                      this.props.history.push("/locationdetails");
+                      // this.props.dispatch({
+                      //   type: "FETCH_LOCATION_DETAILS",
+                      //   payload: rowData.LocationID
+                      // });
+                      this.props.history.push(`/locationdetails/${rowData.LocationID}`);
                     }
                   }
                 ]}
